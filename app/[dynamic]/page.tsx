8ed5cc5f18044dba314bf46dea6f86d4c5f11bbc
Matas Cypas <mcypaszh@gmail.com>
2023-07-27 11:23:54 +0300

    a

--- app/[dynamic]/page.tsx
+++ app/[dynamic]/page.tsx
@@ -1,11 +1,29 @@
-// export async function generateStaticParams() {
-//   const clients = await fetch()
-// }
+export async function generateStaticParams() {
+  const clients = [
+    {
+      client: "morningstar",
+      content: "Yes, hello, I'm Morningstar",
+    },
+    {
+      client: "apollo",
+      content: "yesyesyeyseyseyse",
+    },
+    {
+      client: "crazy8",
+      content: "crazy 8 DDDDDDDDDD",
+    },
+  ];
 
-const Page = (context: any) => {
+  return clients.map(({ client }) => ({
+    dynamic: client,
+  }));
+}
+
+const Page = ({ params }: any) => {
+  console.log(params.dynamic);
   return (
     <div className="w-full h-full flex flex-col justify-center align-middle p-3">
-      <h1>{context.params.dynamic}</h1>
+      {/* <h1>{context.params.dynamic}</h1> */}
     </div>
   );
 };
