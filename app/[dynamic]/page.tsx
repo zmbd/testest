c23bac176ed5af11e08db65e237af6335e6f726a
Matas Cypas <mcypaszh@gmail.com>
2023-07-27 17:56:29 +0300

    Mmeqw

--- app/[dynamic]/page.tsx
+++ app/[dynamic]/page.tsx
@@ -1,16 +1,6 @@
-export const dynamicProps = true;
+"use client";
 
-export async function generateStaticParams() {
-  const clients = await fetch("https://nextjsisrback.onrender.com/clients", {
-    next: { revalidate: 60 },
-  }).then((res) => res.json());
-
-  return clients.map(({ client }: { client: string }) => ({
-    dynamic: client,
-  }));
-}
-
-export default async function Page({ params }: any) {
+export default function Page({ params }: any) {
   return (
     <div className="w-full h-full flex flex-col justify-center align-middle p-3">
       <h1>{params.dynamic}</h1>
