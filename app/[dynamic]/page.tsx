2c8921686ad19fd16d93f8a86235c3ec08c85f27
Matas Cypas <mcypaszh@gmail.com>
2023-07-27 10:53:53 +0300

    teastatasta

--- app/[dynamic]/page.tsx
+++ app/[dynamic]/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+
+import { useState } from "react";
+
 const Page = (context: any) => {
-  return <div>{context.params.dynamic}</div>;
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <div className="w-full h-full flex flex-col justify-center align-middle p-3">
+      <h1>{context.params.dynamic}</h1>
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+    </div>
+  );
 };
 
 export default Page;
