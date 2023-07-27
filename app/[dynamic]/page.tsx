b4e14c3f74db1e23fc02e79388cb0bd1422adaad
Matas Cypas <mcypaszh@gmail.com>
2023-07-27 11:10:00 +0300

    asdada

--- app/[dynamic]/page.tsx
+++ app/[dynamic]/page.tsx
@@ -1,14 +1,11 @@
-"use client";
-
-import { useState } from "react";
+// export async function generateStaticParams() {
+//   const clients = await fetch()
+// }
 
 const Page = (context: any) => {
-  const [count, setCount] = useState<number>(0);
-
   return (
     <div className="w-full h-full flex flex-col justify-center align-middle p-3">
       <h1>{context.params.dynamic}</h1>
-      <button onClick={() => setCount(count + 1)}>{count}</button>
     </div>
   );
 };
