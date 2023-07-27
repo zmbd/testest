690e06c348f5692b4eb85412763d6170d9d59437
Matas Cypas <mcypaszh@gmail.com>
2023-07-27 16:22:10 +0300

    Mm

--- app/[dynamic]/page.tsx
+++ app/[dynamic]/page.tsx
@@ -11,16 +11,6 @@ export async function generateStaticParams() {
 }
 
 export default async function Page({ params }: any) {
-  const clients = await fetch("https://nextjsisrback.onrender.com/clients", {
-    next: { revalidate: 60 },
-  }).then((res) => res.json());
-
-  const isValid = clients.some((client) => client.client === params.dynamic);
-
-  console.log(isValid);
-
-  if (!isValid) return <></>;
-
   return (
     <div className="w-full h-full flex flex-col justify-center align-middle p-3">
       <h1>{params.dynamic}</h1>
