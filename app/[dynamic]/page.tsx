88c0710b5ae6abf25dd2b884ca3476bbe86ce94e
Matas Cypas <mcypaszh@gmail.com>
2023-07-28 09:43:51 +0300

    daslda

--- app/[dynamic]/page.tsx
+++ app/[dynamic]/page.tsx
@@ -1,6 +1,16 @@
-"use client";
+async function getClients() {
+  const res = await fetch("https://nextjsisrback.onrender.com/clients", {
+    next: { revalidate: 60 },
+  });
 
-export default function Page({ params }: any) {
+  const clients = await res.json();
+
+  return clients;
+}
+
+export default async function Page({ params }: any) {
+  const clients = await getClients();
+
   return (
     <div className="w-full h-full flex flex-col justify-center align-middle p-3">
       <h1>{params.dynamic}</h1>
