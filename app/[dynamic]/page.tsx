a8cf227304a7e53be6f25798db12aa304a786cf3
Matas Cypas <mcypaszh@gmail.com>
2023-07-31 13:32:29 +0300

    Add submodule

--- app/[dynamic]/page.tsx
+++ app/[dynamic]/page.tsx
@@ -1,4 +1,8 @@
-async function getClients() {
+async function getClients(requestedClient: string, isPreview: boolean) {
+  // const res = await fetch("https://nextjsisrback.onrender.com/clients", {
+  //   next: { revalidate: isPreview ? 0 : 100 },
+  // });
+
   const res = await fetch("https://nextjsisrback.onrender.com/clients", {
     next: { revalidate: 60 },
   });
@@ -9,10 +13,16 @@ async function getClients() {
 }
 
 export default async function Page({ params }: any) {
-  const clients = await getClients();
+  const clients = await getClients("morningstar", true);
+
+  const isRouteValid = clients.some(
+    (client: any) => client.client === params.dynamic
+  );
+
+  if (!isRouteValid) return <h1>NO</h1>;
 
   return (
-    <div className="w-full h-full flex flex-col justify-center align-middle p-3">
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>{params.dynamic}</h1>
     </div>
   );
